5c36e7137d1c67f6bdb13d303fe7c83483d042a5
Ajay Dasari <ajaykkdasari@gmail.com>
2018-11-20 20:30:16 +0000

    first build

---
+++ src/main/java/HelloWorldMaven/HelloWorldMaven/App.java
@@ -0,0 +1,13 @@
+package HelloWorldMaven.HelloWorldMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
